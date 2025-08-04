ffae23c5b0e108de2707221aa5f3c0097b637e43
Syllock25 <mrs@simcorp.com>
2025-08-04 13:19:46 +0200

    Update streamlit_app.py
    Changed connection call, removed the extra tage "snowflake".

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,15 @@ if name_on_order:
 # Specific change for Streamlit not in Snowflake SniS
 # Establish connection to Snowpart session (!)
 # cnx = st.connection("snowflake")
-cnx = st.connection("snowflake", type="Snowflake", account="KXOVTJG-QLB45630", user="MICHELRS", password="SnowFlake2025+", warehouse="COMPUTE_WH", database="SMOOTHIES", schema="PUBLIC")
+cnx = st.connection(
+                    type="Snowflake", 
+                    account="KXOVTJG-QLB45630", 
+                    user="MICHELRS", 
+                    password="SnowFlake2025+", 
+                    warehouse="COMPUTE_WH", 
+                    database="SMOOTHIES", 
+                    schema="PUBLIC"
+                   )
 
 session = cnx.session()
 
